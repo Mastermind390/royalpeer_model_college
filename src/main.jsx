7b037aef3aa052d1361cae06849d09db8876c999
Enitan Elijah <enitanelijah39@gmail.com>
2026-08-16 20:01:16 +0100

    add P

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  onOfflineReady() {
+    console.log('App ready to work offline')
+  },
+
+  onNeedRefresh() {
+    console.log('New version available')
+  },
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,45 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Royal Peer Model College',
+        short_name: 'Royal Peer',
+        description: 'Royal Peer Model College',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+
+        icons: [
+        {
+          src: 'logo-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'logo-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+      },
+
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg}'],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+      },
+    }),
+
+
+  ],
    base: '/royalpeer/',
 })
 
